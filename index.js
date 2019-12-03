68c471bd819cf257119b958595791c5a83ce508e
Jessica Cooper <jessicagcooper@gmail.com>
2019-12-03 09:54:19 -0600

    Some code.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // implement your API here
+const express = require("express");
+
+const db = require("./data/db.js")
+
+const server = express();
+
+server.use(express.json());
+
+
+//POST
+server.post("/api/users", (req, res) => {
+    const userData = req.body
+
+    if (userData !== "name" && "bio")
+    
+    db.add(userData)
+    .then()
+    .catch(error => {
+        
+    })
+})
+
+
+
+
+
+const port = 6000;
+server.listen(port, () => 
+  console.log(`\n ** API running on ${port} **\n`)
+);
